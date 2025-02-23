ffe82dfe40c156ad6582c2122c9fa091e2bca0f2
ewang620 <jeremia314@gmail.com>
2025-02-23 17:31:27 +0000

    adding declaration for isValid()

---
+++ include/sudoku.h
@@ -0,0 +1,16 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include <vector>
+#include <iostream>
+
+class SudokuBoard {
+public:
+    SudokuBoard();
+    void print() const;
+    bool isValid(int row, int col, int num) const;
+private:
+    std::vector<std::vector<int>> board;
+};
+
+#endif
